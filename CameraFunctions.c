65db84d7ef0bd103a7b4aab879dfc379f7c9a678
ReviloRT <opvannoort@gmail.com>
2022-09-23 12:38:46 +1200

    Slow Improvements and some more detail

--- CameraFunctions.c
+++ CameraFunctions.c
@@ -1,9 +1,34 @@
 
+#define CAM_ROWLENGTH 320
+#define CAM_COLLENGTH 320
+#define CAM_PIXBITS 8
+// TODO: Check 0x24 syntax
+#define CAM_I2CADDRESS 0x24
+#define CAM_WAITTIMEOUT 1000000
+#define CAM_IMAGEFOLDER "imageFolder"
+
+// TODO: Not sure if these are defined like this?
+#define CAM_VSYNC 0
+#define CAM_HSYNC 0
+#define CAM_PIXCLOCK 0
+#define CAM_DATA0 0
+
+// ERROR DEFINES
+#define CAM_SUCCESS 0
+#define CAM_ERROR_INVALIDI2CRESPONSE 1
+#define CAM_ERROR_BUFFERTOOSMALL 2
+#define CAM_ERROR_NORESPONSE 3
+
+// initalises the camera pins and changes required registers via I2C.
 int initCamera(){
+  // Set up the HSYNC, HSYNC, PIXCLOCK and DATA0 Pins
+  // TODO: Figure pin setup procedure
+
+  // USING I2C:
   // check enabled by reading the sensor part number
     // read 0x0000[7:0] should be 0x01 in MODEL_ID_H
     // read 0x0001[7:0] should be 0xB0 in MODEL_ID_L
-    // if failed, return 1
+    // if failed, return CAM_ERROR_INVALIDI2CRESPONSE
 
   // change setting registers \
   // set the data mode to serial
@@ -15,42 +40,66 @@ int initCamera(){
   return 0
 }
 
-// TODO: check how long it takes to save a single bit vs save line vs save image
-void takeImage(uint8 *bufferPointer, int bufferLength){
-  // initalise pixNum couter
+// Sends a request to the camera, then waits for and
+// records the image response to the buffer.
+// Returns success if the image was successfully recorded
+int takeImage(uint8 *bufferPointer[], int bufferLength){
+  // initalise pixel position counters
+  int rowNum = 0;
   int pixNum = 0;
+
+  // Ensure the buffer is large enough
+  if (bufferLength < CAM_ROWLENGTH * CAM_COLLENGTH){
+    return CAM_ERROR_BUFFERTOOSMALL;
+  }
 
   // Send request over I2C
   requestImage();
 
   // Poll until the VSYNC pin goes high
-  while (!VSYNC) {}
+  int timeOut = 0;
+  while (!CAM_VSYNC) {
+    timeOut ++;
+    if (CAM_WAITTIMEOUT < timeOut){
+      return CAM_ERROR_NORESPONSE;
+    }
+  }
 
   // while frame is being transfered (VSYNC high)
-  while (VSYNC) {
-  // while transfering a line of pix (HSYNC high)
-    while (HSYNC) {
+  while (CAM_VSYNC) {
+  // while transfering a line of pixels (HSYNC high)
+    pixNum = 0;
+    while (CAM_HSYNC) {
       // gather a single pixel, 8 cycles
-      int byte = 0; // TODO: figure Datatype
-      for (size_t i = 0; i < 8; i++) {
+      uint8 byte = 0;
+      for (size_t i = 0; i < CAM_PIXBITS; i++) {
         // wait for the centre of the clock cycle
-        // waitForPXClockCycle(0);
-        // add serial bit to byte
-        // byte | something
+        if (waitForPXClockChangeTo(0) != CAM_SUCCESS){
+          return CAM_ERROR_NORESPONSE;
+        }
+        // Read data0 pin and
+        // or the value with the correct position in byte
+        // TODO: Figure BIT[i]
+        byte = byte | (BIT[i]&CAM_DATA0);
       }
-      // save byte to SD, or add byte to line buffer
+      bufferPointer[rowNum*CAM_ROWLENGTH + pixNum] = byte;
+      pixNum ++;
     }
-    // write new line, or add line to frame buffer
+    rowNum ++;
   }
-  // write frame if need be
+
+  return CAM_SUCCESS;
 }
 
-void saveImage(folderName, bufferPointer, bufferLength){
-  // gather an image line into a binary string
+// Save the image to the
+void saveImage(int imageID, uint8 *bufferPointer, int bufferLength){
+  // gather an image line into a binary string???
 
   // write that binary string into the SD card
+  // CAM_IMAGEFOLDER
 }
 
+// Sends an I2C message to the camera to take a photo
 void requestImage(){
   // Address = 0x24
   // Trigger Message
@@ -60,7 +109,28 @@ void requestImage(){
     // 0x0100[2:0] = 011
 }
 
-void waitForPXClockCycle(bool level){
-  // while the PXpin == level
-    // do nothing
+// Polls until the PXClock changes to the given level
+// if already at level, waits for it to cycle to level
+int waitForPXClockChangeTo(bool level){
+  // Check that we are not already at level
+  int timeOut = 0;
+  if (CAM_PXCLOCK == level) {
+    // If we are, wait until the pin is different to level
+    while (CAM_PXCLOCK == level){
+      // Wait until timeout cycles
+      timeOut ++;
+      if (CAM_WAITTIMEOUT < timeOut){
+        return CAM_ERROR_NORESPONSE;
+      }
+    }
+  }
+  timeOut = 0;
+  // Wait until the pin is the same as level
+  while (CAM_PXCLOCK != level){
+    timeOut ++;
+    if (CAM_WAITTIMEOUT < timeOut){
+      return CAM_ERROR_NORESPONSE;
+    }
+  }
+  return CAM_SUCCESS;
 }
