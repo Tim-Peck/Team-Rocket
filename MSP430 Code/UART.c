7bdcc87acaf6b8fda72352a1c8cb2143271ed6bc
knyu <knyu88@gmail.com>
2022-11-13 23:38:06 +1300

    initial working code for test  MSP430, including I2C with test sensors and UART for sending to serial monitor

---
+++ MSP430 Code/UART.c
@@ -0,0 +1,83 @@
+#include <msp430.h>
+#include <inttypes.h>
+#include "UART.h"
+
+void uart_init() {
+    next_idx_to_send_UART = 0;
+    next_idx_to_store_UART = 0;
+    current_length_UART = 0;
+	
+	// Configure pins
+    P1SEL0 |= BIT5 | BIT4; // P1.5=RXD, P1.4=TXD
+
+    // Configure UART
+    UCA0CTLW0 |= UCSWRST;       // Put UART state machine in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK; // Choose SMCLK as clock source
+
+    // Baud Rate calculation - we want 9600 baud
+    // N = clock / baud = 1,048,576 Hz / 9600 = 109.227
+    // N > 16 so UCOS16 = 1
+    // UCA0BR0 = int(N / 16) = int(109.227 / 16) = int(6.827) = 6
+    // UCBRSx  = 0x22 as per users guide table "UCBRSx settings"
+    // UCBRFx  = int([(N / 16) - int(N / 16)] * 16) = int(13.227) = 8
+
+    UCA0BRW = 6;
+    UCA0MCTLW = (0x22 << 8) | UCBRF_13 | UCOS16;
+
+
+    UCA0CTLW0 &= ~UCSWRST;      // Initialize UART state machine
+}
+
+void uart_send_byte(byte) {
+    if (current_length_UART < BUFFER_SIZE) {
+        msg_buffer[next_idx_to_store_UART++] = byte; // next_idx is always incremented and not reset
+        current_length_UART++; // so current length alternates between 0 and 1 for each byte
+    }
+
+    // enable transmit interrupt to start transmitting
+    // if we are already transmitting this does nothing
+    UCA0IE |= UCTXIE;
+}
+
+void uart_send_bytes(uint8_t *bytes, uint8_t number_of_bytes) {
+    uint8_t i;
+    for (i = 0; i < number_of_bytes; i++) {
+        uart_send_byte(bytes[i]);
+    }
+}
+
+
+// ISR TO USCI_A0
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void) {
+    switch(UCA0IV) {
+        case USCI_NONE: break;
+        case USCI_UART_UCRXIFG: break;
+
+        case USCI_UART_UCTXIFG:
+            if (current_length_UART > 0) {
+                UCA0TXBUF = msg_buffer[next_idx_to_send_UART++];
+                current_length_UART--;
+            }
+            else {
+                // Entering the interrupt service routine (ISR), will clear
+                // interrupt flags. i.e. we have just cleared UCTXIFG
+                // UCTXIFG is usually set when UCA0TXBUF is emptied. However we
+                // didn't write anything to the buffer this time, so it's empty
+                // and cannot be "emptied". i.e. UCTXIFG will not get set again
+                // We can however set UCTXIFG manually to re-enter the ISR!
+                // Doing this outside of the ISR is dangerous because we may be
+                // in the middle of a UART transmission. Instead, we can set it
+                // here and disable TX interrupts. When interrupts are enabled
+                // again, UCTXIFG will still be set so we will immediately come
+                // back here.
+                UCA0IE &= ~UCTXIE;
+                UCA0IFG |= UCTXIFG;
+            }
+            break;
+
+        case USCI_UART_UCSTTIFG: break;
+        case USCI_UART_UCTXCPTIFG: break;
+        default: break;
+    }
+}
---
+++ MSP430 Code/main.c
@@ -0,0 +1,49 @@
+// 13/11/2022
+// Last change: Kenny Yu
+// Current function: displays received byte/s on serial monitor with UART from I2C on test microcontroller with test sensors
+
+#include <msp430.h>
+#include <inttypes.h>
+#include "I2C.h"
+#include "UART.h"
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5; // disable the GPIO power-on default high-impedance mode
+                          // to activate previously configured port settings
+
+    __enable_interrupt();  // enable global interrupts
+    __delay_cycles(90000); // GIE (or watchdog?) needs time before set
+
+    // ------------------------- DO NOT CHANGE ABOVE ---------------------------- //
+
+    uint8_t receiveData[10];
+
+    // set direction of pin 1.0 for led
+    P1DIR |= BIT0;
+
+    uart_init();
+    i2c_init();
+
+    accelInit();
+    barInit();
+
+//    getAccel(receiveDataData);
+//    getBar(receiveData);
+    getByte(0x76, 0xD0, receiveData);
+
+    uart_send_byte(receiveData[0]);
+//    uart_send_byte(receiveData[1]);
+
+    // keep MSP running
+    while (1)
+    {
+//        __delay_cycles(100000);
+//        getAccel(receiveData);
+//        uart_send_byte(receiveData[0]);
+//        uart_send_byte(receiveData[1]);
+    }
+
+    return 0;
+}
